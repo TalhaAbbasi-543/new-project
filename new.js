3175fac55fd873b2cbf410e73d6e5cbbde55d49b
Talha <talhaabbaci543@gmail.com>
2026-01-03 23:50:51 +0500

    Operater Code

--- new.js
+++ new.js
@@ -1,1 +1,18 @@
 console.log("My Name is Talha Abbasi !");
+ let a = 5;
+ if (a)
+ {
+   console.log("True Value");
+ }
+ else{
+  console.log("False Value");
+ }
+ let result = 0  ? 'true' : 'false'
+
+ let age = 15;
+ if(age>=10){
+  console.log('You are Valid for Driving License');
+ }
+ else {
+     console.log('You are Not Valid for Driving License');
+ }
